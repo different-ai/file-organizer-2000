7da3a155ef4eb3af866936d327c4470129245c2a
Benjamin Shafii <ben@prologe.io>
2024-04-09 21:48:05 +0200

    feat(api/audio): increase size

--- app/pages/api/audio.ts
+++ app/pages/api/audio.ts
@@ -6,27 +6,40 @@ import { tmpdir } from "os";
 import { join } from "path";
 import { promises as fsPromises } from "fs";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "POST") {
-        const { file } = req.body;
-        const base64Data = file.split(';base64,').pop();
-        const tempFilePath = join(tmpdir(), `upload_${Date.now()}.mp3`);
-        await fsPromises.writeFile(tempFilePath, base64Data, { encoding: 'base64' });
+export const config = {
+	api: {
+		bodyParser: {
+			sizeLimit: "20mb",
+		},
+	},
+};
 
-        const apiKey = process.env.OPENAI_API_KEY;
-        const openai = new OpenAI({ apiKey, dangerouslyAllowBrowser: true });
-        const transcription = await openai.audio.transcriptions.create({
-            file: fs.createReadStream(tempFilePath),
-            model: "whisper-1",
-        });
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	if (req.method === "POST") {
+		const { file } = req.body;
+		const base64Data = file.split(";base64,").pop();
+		const tempFilePath = join(tmpdir(), `upload_${Date.now()}.mp3`);
+		await fsPromises.writeFile(tempFilePath, base64Data, {
+			encoding: "base64",
+		});
 
-        // Clean up the temporary file
-        await fsPromises.unlink(tempFilePath);
+		const apiKey = process.env.OPENAI_API_KEY;
+		const openai = new OpenAI({ apiKey, dangerouslyAllowBrowser: true });
+		const transcription = await openai.audio.transcriptions.create({
+			file: fs.createReadStream(tempFilePath),
+			model: "whisper-1",
+		});
 
-        res.status(200).json({ text: transcription.text });
-    } else {
-        // Handle any non-POST requests
-        res.setHeader("Allow", ["POST"]);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+		// Clean up the temporary file
+		await fsPromises.unlink(tempFilePath);
+
+		res.status(200).json({ text: transcription.text });
+	} else {
+		// Handle any non-POST requests
+		res.setHeader("Allow", ["POST"]);
+		res.status(405).end(`Method ${req.method} Not Allowed`);
+	}
 }
