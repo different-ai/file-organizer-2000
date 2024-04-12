61f2f19e3eb7ff68be460fa780a2b701ea5cd972
aexshafii <alexandre.shafii@gmail.com>
2024-04-12 14:31:37 +0800

    fix: fix tagging misbehavior

--- src/modules/text.ts
+++ src/modules/text.ts
@@ -2,36 +2,37 @@ import { requestUrl } from "obsidian";
 import { logMessage } from "../../utils";
 
 async function useText(content: string, systemPrompt: string) {
-	const data = {
-		model: "gpt-4-1106-preview",
-		temperature: 0,
-		messages: [
-			{
-				role: "system",
-				content: systemPrompt,
-			},
-			{
-				role: "user",
-				content: content,
-			},
-		],
-	};
-	const baseUrl = "https://file-organizer-2000.vercel.app/";
-	const endpoint = "api/text";
-	const url = `${baseUrl}/${endpoint}`;
+  const data = {
+    model: "gpt-4-1106-preview",
+    temperature: 0,
+    messages: [
+      {
+        role: "system",
+        content: systemPrompt,
+      },
+      {
+        role: "user",
+        content: content,
+      },
+    ],
+  };
+  const baseUrl = "https://file-organizer-2000.vercel.app/";
+  const endpoint = "api/text";
+  const url = `${baseUrl}/${endpoint}`;
 
-	const response = await requestUrl({
-		url: url,
-		method: "POST",
-		body: JSON.stringify(data),
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
-	const result = await response.json;
-	logMessage(result);
-	logMessage(result.choices[0].message.content);
-	return result.choices[0].message.content.trim();
+  const response = await requestUrl({
+    url: url,
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const result = await response.json;
+  logMessage(result);
+  logMessage(result.choices[0].message.content);
+  // remove all special characters except # to avoid having tags item listed with - or other special characters
+  return result.choices[0].message.content.trim();
 }
 
 export default useText;
