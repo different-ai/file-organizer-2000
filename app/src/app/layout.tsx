af8149a3c98ac3e0915032f44fc60d9a7ba73553
Benjamin Shafii <ben@prologe.io>
2024-04-13 23:12:10 +0200

    feat(app): add auth

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,12 +12,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+     </ClerkProvider>
   );
 }
