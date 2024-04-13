6247950b18081b46fc8b7d7c942b9c326ee83c86
Benjamin Shafii <ben@prologe.io>
2024-04-14 00:49:46 +0200

    fix(clerk): use correct middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: "/api/*",
+  publicRoutes: ["/api/name", "/api/vision", "/api/audio", "/api/text"],
 });
 
 export const config = {
