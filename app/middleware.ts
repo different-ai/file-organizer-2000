aeead9c3cf9e38460190cbf2fd723b50a97087cf
aexshafii <alexandre.shafii@gmail.com>
2024-04-15 20:05:28 +0800

    chore: typed  middleware arguments

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,13 +1,12 @@
 import { authMiddleware as clerkAuthMiddleware } from "@clerk/nextjs";
-
+import { NextFetchEvent, NextRequest } from 'next/server';
 
 const authMiddleware = clerkAuthMiddleware({
   publicRoutes: ["/api/name", "/api/vision", "/api/audio", "/api/text"],
 });
 
 // only use authMiddleware if ENABLE_USER_MANAGEMENT is true, i.e. if user wishes to self host
-export default function middleware(req, res
-) {
+export default function middleware(req: NextRequest, res: NextFetchEvent) {
   console.log("ENABLE_USER_MANAGEMENT", process.env.ENABLE_USER_MANAGEMENT)
 
   if (process.env.ENABLE_USER_MANAGEMENT == "true") {
