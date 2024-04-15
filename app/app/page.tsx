63baace462783334e693112e237a600782cd77cd
aexshafii <alexandre.shafii@gmail.com>
2024-04-15 20:46:29 +0800

    feat: show different instructions on frontend Home screen when self-hosting

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,10 +5,18 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold">Welcome to File Organizer 2000</h1>
+        { process.env.ENABLE_USER_MANAGEMENT == 'true' ? ( 
+        <div>
+          <p className="text-xl mt-4">
+            Just paste this inside File Organizer 2000 and you're good to go!
+          </p>
+          <UnkeyElements />
+        </div>
+        ) :  (
         <p className="text-xl mt-4">
-          Just paste this inside File Organizer 2000 and you're good to go!
+          Just paste this URL in the plugin settings in Obsidian and you're good to go!
         </p>
-        <UnkeyElements />
+        )}
       </div>
     </main>
   );
