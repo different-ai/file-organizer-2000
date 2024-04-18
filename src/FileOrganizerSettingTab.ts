a51b3f46f79ab2830d6dbe6aa2b6b533aab5d390
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2024-04-18 17:22:36 +0800

    fix(plugin): only send early access code when enough chars
    ---------
    
    Co-authored-by: ben <ben@prologe.io>

--- src/FileOrganizerSettingTab.ts
+++ src/FileOrganizerSettingTab.ts
@@ -177,6 +177,9 @@ export class FileOrganizerSettingTab extends PluginSettingTab {
           .setPlaceholder("Enter access code for Early Access Features")
           .setValue(this.plugin.settings.earlyAccessCode)
           .onChange(async (value) => {
+                       if (value.length !== 8) {
+              return;
+            }
             const jsonPayload = {
               code: value,
             };
@@ -193,14 +196,14 @@ export class FileOrganizerSettingTab extends PluginSettingTab {
               });
               logMessage(response);
 
-              if (response.status === 200) {
-                this.plugin.settings.earlyAccessCode = value;
-                this.plugin.settings.enableEarlyAccess = true; // Assuming this setting enables all early access features
-                await this.plugin.saveSettings();
-                new Notice("Early Access Features Activated Successfully");
-              } else {
+              if (response.status !== 200) {
                 new Notice("Failed to activate Early Access Features.");
+                return;
               }
+              this.plugin.settings.earlyAccessCode = value;
+              this.plugin.settings.enableEarlyAccess = true; // Assuming this setting enables all early access features
+              await this.plugin.saveSettings();
+              new Notice("Early Access Features Activated Successfully");
             } catch (error) {
               console.error("Error activating Early Access Features:", error);
               new Notice("Error during activation process.");
