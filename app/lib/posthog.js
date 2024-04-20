f2f90d0aac2b21f26f756a92ba792f9ede8c4bd1
Benjamin Shafii <ben@prologe.io>
2024-04-20 10:38:25 +0200

    fix: vercel build error

--- app/lib/posthog.js
+++ app/lib/posthog.js
@@ -4,16 +4,11 @@ import { PostHog } from 'posthog-node'
 export default function PostHogClient() {
     if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
         return null
-    } 
+    }
     const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
         host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
         flushAt: 1,
         flushInterval: 0
     })
     return posthogClient
-}`
-
-
-
-
-
+}
