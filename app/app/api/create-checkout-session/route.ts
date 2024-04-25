e6064d5159d3ffd837f5384dd6c05faa1a671275
Benjamin Shafii <ben@prologe.io>
2024-04-25 13:33:35 +0200

    fix version

--- app/app/api/create-checkout-session/route.ts
+++ app/app/api/create-checkout-session/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 import { auth } from '@clerk/nextjs';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-04-10',
 });
 
 export async function POST(req: NextRequest) {
