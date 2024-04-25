d69cba2496602e5c66f92d81e39652c2f8fd8d10
Benjamin Shafii <ben@prologe.io>
2024-04-25 15:47:56 +0200

    add more console logs

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,38 +1,40 @@
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-    publicRoutes: (req) => req.url.includes("/api"),
+  publicRoutes: (req) => req.url.includes("/api"),
   afterAuth: async (auth, req) => {
-    console.log('in after auth')
+    console.log("in after auth");
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
     }
 
-      console.log('auth claim',auth.sessionClaims.publicMetadata)
+    console.log(auth.userId);
+    console.log("auth claim", auth.sessionClaims);
+    console.log("auth claim", auth.sessionClaims.publicMetadata);
     if (
       auth.userId &&
-      req.nextUrl.pathname === '/members' &&
+      req.nextUrl.pathname === "/members" &&
       // @ts-ignore
-      auth.sessionClaims.publicMetadata?.stripe?.payment !== 'paid'
+      auth.sessionClaims.publicMetadata?.stripe?.payment !== "paid"
     ) {
-      return NextResponse.redirect(new URL('/', req.url));
+      return NextResponse.redirect(new URL("/", req.url));
     }
-    console.log('new')
+    console.log("new");
     if (
       auth.userId &&
-      req.nextUrl.pathname === '/members' &&
+      req.nextUrl.pathname === "/members" &&
       // @ts-ignore
-      auth.sessionClaims.publicMetadata?.stripe?.payment === 'paid'
+      auth.sessionClaims.publicMetadata?.stripe?.payment === "paid"
     ) {
       return NextResponse.next();
     }
-    console.log('members')
-    if (auth.userId && req.nextUrl.pathname !== '/members') {
+    console.log("members");
+    if (auth.userId && req.nextUrl.pathname !== "/members") {
       return NextResponse.next();
     }
     if (auth.isPublicRoute) {
-      console.log(' is public route')
+      console.log(" is public route");
       return NextResponse.next();
     }
   },
