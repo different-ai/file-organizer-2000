266c79e91d67dfb6995f1ad81443dd897397e891
aexshafii <alexandre.shafii@gmail.com>
2024-04-26 20:55:53 +0800

    chore: refactored for easier understanding

--- src/FileOrganizerSettingTab.ts
+++ src/FileOrganizerSettingTab.ts
@@ -230,23 +230,23 @@ export class FileOrganizerSettingTab extends PluginSettingTab {
     new Setting(containerEl)
       .setName("Apply AI Template Formatting (early access only)")
       .setDesc("Automatically format your documents using your AI templates (when applicable) during file processing.")
-      .addToggle((toggle) =>
-        toggle
-          .setValue(this.plugin.settings.aiTemplateFormatting)
-          .onChange(async (value) => {
-            this.plugin.settings.aiTemplateFormatting = value && this.plugin.settings.enableEarlyAccess;
-
-            if (value && !this.plugin.settings.enableEarlyAccess) {
-              new Notice(
-                "You need to be a supporter to enable this feature.",
-                6000
-              );
-              toggle.setValue(false);
-              this.plugin.settings.aiTemplateFormatting = false;
-            }
-            await this.plugin.saveSettings();
-          })
-      );
+      .addToggle((toggle) => {
+        toggle.setValue(this.plugin.settings.aiTemplateFormatting);
+        toggle.onChange(async (value) => {
+          const enableEarlyAccess = this.plugin.settings.enableEarlyAccess;
+          const canEnableFeature = enableEarlyAccess || !value;
+          if (canEnableFeature) {
+            this.plugin.settings.aiTemplateFormatting = value;
+          } else {
+            new Notice(
+              "You need to be a supporter to enable this feature.",
+              6000
+            );
+            toggle.setValue(false);
+          }
+          await this.plugin.saveSettings();
+        });
+      });
 
     new Setting(containerEl)
       .setName("Use Self-hosted")
