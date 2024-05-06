c763d45ef7563b65889eb24fb52acc4a1c257db1
Benjamin Shafii <ben@prologe.io>
2024-05-06 22:09:17 +0200

    feat: recude trial time

--- app/app/api/create-checkout-session/route.ts
+++ app/app/api/create-checkout-session/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest) {
     console.log("Creating checkout session for user", userId);
     const session = await stripe.checkout.sessions.create({
       subscription_data: {
-        trial_period_days: 14,
+        trial_period_days: 3,
       },
 
       line_items: [
