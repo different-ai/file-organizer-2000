e73dc03e843258b7672cd7f075970e8f961d0af8
Benjamin Shafii <ben@prologe.io>
2024-05-08 18:46:14 +0200

    fix: early access

--- app/app/api/early-access/route.ts
+++ app/app/api/early-access/route.ts
@@ -23,55 +23,7 @@ async function findCustomerByEmail(email: string) {
 }
 
 export async function POST(request: Request) {
-  if (process.env.ENABLE_USER_MANAGEMENT !== "true") {
-    return new Response(JSON.stringify({ isCustomer: true }), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-
-  // Get the authorization header
-  const header = request.headers.get("authorization");
-  if (!header) {
-    return new Response(JSON.stringify({ isCustomer: false }), {
-      status: 401,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-
-  // Extract the API key from the header
-  const token = header.replace("Bearer ", "");
-
-  // Verify the API key
-  const { result, error } = await verifyKey(token);
-  if (error) {
-    console.error(error.message);
-    return new Response(JSON.stringify({ isCustomer: false }), {
-      status: 401,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-
-  // Get the user ID from the API key
-  const userId = result.ownerId;
-
-  // Get the user object from Clerk
-  const user = await clerkClient.users.getUser(userId);
-  const customer = await findCustomerByEmail(
-    user.emailAddresses[0].emailAddress
-  );
-  console.log("customer", customer);
-  const isCustomer =
-    (customer !== null &&
-      customer.subscriptions.data.some(
-        (subscription) => subscription.status === "active"
-      )) ||
-    // @ts-ignore
-    user.publicMetadata?.stripe?.status === "complete";
-
-  console.log("isCustomer", isCustomer, userId);
-
-  return new Response(JSON.stringify({ isCustomer }), {
+  return new Response(JSON.stringify({ isCustomer: true }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   });
