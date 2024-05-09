7a8a27bd947b3baa9f0cd14ab18a8e0ea507c49a
Benjamin Shafii <ben@prologe.io>
2024-05-09 18:22:33 +0200

    feat(name): update prompt

--- app/app/api/(ai)/name/prompt.ts
+++ app/app/api/(ai)/name/prompt.ts
@@ -1,9 +1,9 @@
 export function generatePrompt(model: string, document: string): string {
-    switch (model) {
-      case "gpt-4-turbo":
-      default:
-        return `Give a name to this document:
-  ${document} should only be 30 chars long max. only answer with the name nothing else.`;
-    }
+  switch (model) {
+    case "gpt-4-turbo":
+    default:
+      return `You are a helpful assistant. You only answer short (less than 30 chars titles). You do not use any special character just text. Use something very specific to the content not a generic title.
+Give a title to this document:
+${document}`;
   }
-  
+}
