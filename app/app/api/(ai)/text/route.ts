bbd01c2f4599f9883345e9fc7836eb40f6eb6be7
Benjamin Shafii <ben@prologe.io>
2024-05-11 10:54:33 +0200

    feat: assume markwon in formatting

--- app/app/api/(ai)/text/route.ts
+++ app/app/api/(ai)/text/route.ts
@@ -19,6 +19,7 @@ export async function POST(request: Request) {
       Text to format:
       ${content}
       Respond with only the formatted text.`,
+      system: "assume markdown",
     });
 
     return NextResponse.json({ message: result.object.fromattedText });
