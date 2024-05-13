ce1be2daad8924a4462f9928d23da38ccac4cacd
Benjamin Shafii <ben@prologe.io>
2024-05-13 22:08:31 +0200

    fix: run issues

--- src/SkeletonLoader.tsx
+++ src/SkeletonLoader.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import "./SkeletonLoader.css"; // Add appropriate styles for the skeleton loader
 
 const SkeletonLoader: React.FC<{ width?: string; height?: string }> = ({
   width = "100%",
