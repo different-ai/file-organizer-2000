1a3f93196e138246dc1733f1ae72c8a1bca5ba6f
Benjamin Shafii <ben@prologe.io>
2024-05-23 13:53:41 +0200

    fix: add missing package

--- app/app/api/(newai)/classify1/route.ts
+++ app/app/api/(newai)/classify1/route.ts
@@ -1,9 +1,16 @@
 // app/app/api/(ai)/classify/route.ts
+import { openai } from "@ai-sdk/openai";
 import { classifyDocument } from "../../../../aiService";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const { content, fileName, templateNames } = await request.json();
-  const documentType = await classifyDocument(content, fileName, templateNames);
+  const model = openai("gpt-4o");
+  const documentType = await classifyDocument(
+    content,
+    fileName,
+    templateNames,
+    model
+  );
   return NextResponse.json({ documentType });
 }
