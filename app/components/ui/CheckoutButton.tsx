9c6740b904ac64b660b7116b511a0513a155a516
aexshafii <alexandre.shafii@gmail.com>
2024-06-05 16:59:17 +0800

    feat: retrieve plan type from query params

--- app/components/ui/CheckoutButton.tsx
+++ app/components/ui/CheckoutButton.tsx
@@ -14,13 +14,25 @@ export default function CheckoutButton() {
   const handleCheckout = async () => {
     setLoading(true);
 
+    // get plan type from query params
+    const { searchParams } = new URL(window.location.href);
+    const planType = searchParams.get("planType");
+    let priceId;
+
+    // get priceId based on planType
+    if (planType === "yearly") {
+      priceId = process.env.NEXT_PUBLIC_STRIPE_YEARLY_PRICE_ID;
+    } else {
+      priceId = process.env.NEXT_PUBLIC_STRIPE_MONTHLY_PRICE_ID;
+    }
+
     const response = await fetch("/api/create-checkout-session", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        priceId: process.env.NEXT_PUBLIC_STRIPE_MONTHLY_PRICE_ID,
+        priceId: priceId,
       }),
     });
 
