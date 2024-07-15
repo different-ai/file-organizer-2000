a8236428d268a49c864dc1032d27c29d6e53cfaf
aexshafii <alexandre.shafii@gmail.com>
2024-07-15 18:26:51 +0200

    feat: enable reverse proxy

--- app/app/providers.tsx
+++ app/app/providers.tsx
@@ -5,7 +5,8 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
     person_profiles: "identified_only",
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     capture_pageleave: true, // Enable pageleave capture
