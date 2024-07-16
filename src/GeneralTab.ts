00b88ef501772dcb3cb21e53a12a867a1f7fdaae
aexshafii <alexandre.shafii@gmail.com>
2024-07-16 15:57:19 +0200

    style: clean up pro settings tab

--- src/GeneralTab.ts
+++ src/GeneralTab.ts
@@ -19,91 +19,38 @@ export class ModelTab {
     // add margin top to the tab content
 
     // title
-    modelTabContent.createEl("h6", { text: "Pro Settings" });
     // Pro Account Toggle
-    const proAccountEl = modelTabContent.createEl("div");
-    proAccountEl.createEl("div", { cls: "separation-bar" });
 
-    new Setting(proAccountEl)
-      .setName("Use Pro Account")
-      .setDesc(
-        "Enable this to route use your api key from File Organizer 2000."
-      )
-      .addToggle((toggle) =>
-        toggle.setValue(this.plugin.settings.usePro).onChange(async (value) => {
-          this.plugin.settings.usePro = value;
-          await this.plugin.saveSettings();
-          this.updateSettingsVisibility();
-          this.plugin.initalizeModels();
-        })
-      );
     const fileOrganizerSettingsEl = modelTabContent.createEl("div", {
       cls: "file-organizer-settings",
-    });
-
-    const loginButton = fileOrganizerSettingsEl.createEl("button", {
-      text: "Login to File Organizer",
-      cls: "file-organizer-login-button",
-    });
-    loginButton.addEventListener("click", () => {
-      window.open("https://app.fileorganizer2000.com", "_blank");
     });
 
     // File Organizer Settings Section
 
     new Setting(fileOrganizerSettingsEl)
-      .setName("File Organizer API Key")
+      .setName("File Organizer Serial Key")
+      .setDesc(
+        "Login to start a free trial to generate a key and paste it here."
+      )
       .addText((text) =>
         text
-          .setPlaceholder("Enter your File Organizer API Key")
+          .setPlaceholder("Enter your File Organizer Serial Key")
           .setValue(this.plugin.settings.API_KEY)
           .onChange(async (value) => {
             this.plugin.settings.API_KEY = value;
             await this.plugin.saveSettings();
           })
       );
-
-    // DONT THINK WE NEED THIS ANYMORE, WE HAVE THE LOGIN BUTTON
-    // Special message for File Organizer
-    // const fileOrganizerMessageEl = fileOrganizerSettingsEl.createEl("div", {
-    //   cls: "file-organizer-message",
-    // });
-    // fileOrganizerMessageEl
-    //   .createEl("p", {
-    //     text: "Access your dashboard at ",
-    //   })
-    //   .createEl("a", {
-    //     href: "https://app.fileorganizer2000.com",
-    //     text: "File Organizer Dashboard",
-    //     target: "_blank",
-    //   });
-
-    // OpenAI Settings Section
-    const openAISettingsEl = modelTabContent.createEl("div", {
-      cls: "openai-settings",
+    fileOrganizerSettingsEl.style.marginTop = "1rem";
+    const loginButton = fileOrganizerSettingsEl.createEl("button", {
+      text: "Login to File Organizer",
+      cls: "file-organizer-login-button",
     });
-    // add separation bar
-    openAISettingsEl.createEl("div", { cls: "separation-bar" });
-    // add OpenAI title
-
-    this.updateSettingsVisibility();
+    loginButton.style.marginTop = "1rem";
+    loginButton.addEventListener("click", () => {
+      window.open("https://app.fileorganizer2000.com", "_blank");
+    });
 
     return modelTabContent;
-  }
-
-  private updateSettingsVisibility(): void {
-    const isPro = this.plugin.settings.usePro;
-    const fileOrganizerSettingsEl = this.containerEl.querySelector(
-      ".file-organizer-settings"
-    );
-    const openAISettingsEl = this.containerEl.querySelector(".openai-settings");
-
-    if (fileOrganizerSettingsEl) {
-      fileOrganizerSettingsEl.style.display = isPro ? "block" : "none";
-    }
-
-    if (openAISettingsEl) {
-      openAISettingsEl.style.display = isPro ? "none" : "block";
-    }
   }
 }
