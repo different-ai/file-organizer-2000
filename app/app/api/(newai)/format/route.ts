68a208412098d2673bbcd7cdf610afaafc6e36dc
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2024-07-18 18:31:21 +0200

    feat: increase maxDuration format

--- app/app/api/(newai)/format/route.ts
+++ app/app/api/(newai)/format/route.ts
@@ -4,6 +4,8 @@ import { incrementAndLogTokenUsage } from "@/lib/incrementAndLogTokenUsage";
 import { handleAuthorization } from "@/lib/handleAuthorization";
 import { getModel } from "@/lib/models";
 
+export const maxDuration = 60; // This function can run for a maximum of 5 seconds
+
 export async function POST(request: NextRequest) {
   try {
     const { userId } = await handleAuthorization(request);
