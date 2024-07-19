b171f553242b6eaa2d8642a5ff0677f081e694cc
aexshafii <alexandre.shafii@gmail.com>
2024-07-19 15:12:29 +0200

    feat: add gpt-4-mini model

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -3,6 +3,7 @@ import { openai } from "@ai-sdk/openai";
 
 const models = {
   "gpt-4o": openai("gpt-4o"),
+  "gpt-4o-mini": openai("gpt-4o-mini"),
   "claude-3-5-sonnet-20240620": anthropic("claude-3-5-sonnet-20240620"),
 };
 
@@ -12,5 +13,6 @@ export const getModel = (name: string) => {
     console.log(`Defaulting to gpt-4o`);
     return models["gpt-4o"];
   }
+  console.log(`Using model ${models[name]}`);
   return models[name];
 };
