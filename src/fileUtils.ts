1d5674c4a6d018522fc70f8c09956ab3c5a6da8e
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2024-07-29 15:45:11 +0200

    feat: remove transcription from formatted meetings (#169)

--- src/fileUtils.ts
+++ src/fileUtils.ts
@@ -29,12 +29,10 @@ export async function checkAndCreateTemplates(
   if (!(await app.vault.adapter.exists(meetingNoteTemplatePath))) {
     await app.vault.create(
       meetingNoteTemplatePath,
-      `# Meeting Note Template
-
-## Meeting Details
+      `## Meeting Details
 
 - Date: {{date}} in format YYYY-MM-DD
-
+- Participants: {{participants}}
 
 ## Audio Reference
 
@@ -75,9 +73,8 @@ AI Instructions:
    - Use bullet points and sub-bullets for detailed points under each topic.
    - Preserve the granularity of the discussion, including specific examples or minor points.
 5. Preserve the reference to the original audio file.
-6. Keep the full transcription at the bottom of the note for reference.
-7. Maintain the overall structure of the note, including all headers and sections.
-`
+6. Maintain the overall structure of the note, including all headers and sections.
+7. Delete transcription. Mention that it can be accessed in the Original file`
     );
   }
 }
