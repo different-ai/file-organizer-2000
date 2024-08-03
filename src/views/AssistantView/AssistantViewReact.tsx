b6f67bd0af42650e25f5c82b9c01c17564836507
aexshafii <alexandre.shafii@gmail.com>
2024-08-03 14:59:01 +0200

    feat/style: use 500 obsidian popular tags for reference + styling refresh button

--- src/views/AssistantView/AssistantViewReact.tsx
+++ src/views/AssistantView/AssistantViewReact.tsx
@@ -669,7 +669,7 @@ export const AssistantView: React.FC<AssistantViewProps> = ({ plugin }) => {
             >
               <path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 1 18.8-4.3M22 12.5a10 10 0 0 1-18.8 4.3" />
             </svg>
-            <span>Refresh Tags</span>
+  
           </button>
         }
       />
