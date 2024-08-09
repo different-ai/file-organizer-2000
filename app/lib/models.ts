c3e66c64083f39d99fd60ae7daa6c28ed0affb11
aexshafii <alexandre.shafii@gmail.com>
2024-08-09 12:57:14 +0200

    feat: log model name correctly

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -2,8 +2,8 @@ import { anthropic } from "@ai-sdk/anthropic";
 import { openai } from "@ai-sdk/openai";
 
 const models = {
-  "gpt-4o-2024-08-06": openai("gpt-4o-2024-08-06"),
   "gpt-4o": openai("gpt-4o"),
+  "gpt-4o-2024-08-06": openai("gpt-4o-2024-08-06"),
   "gpt-4o-mini": openai("gpt-4o-mini"),
   "claude-3-5-sonnet-20240620": anthropic("claude-3-5-sonnet-20240620"),
 };
@@ -14,6 +14,6 @@ export const getModel = (name: string) => {
     console.log(`Defaulting to gpt-4o-2024-08-06`);
     return models["gpt-4o-2024-08-06"];
   }
-  console.log(`Using model ${models[name]}`);
+  console.log(`Using model ${name}`);
   return models[name];
 };
