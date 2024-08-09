3d8dbc203d0ee35f9615f3abb7d127c9652c6d18
aexshafii <alexandre.shafii@gmail.com>
2024-08-09 12:37:39 +0200

    feat: default to latest gpt-4o model for vision

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -2,6 +2,7 @@ import { anthropic } from "@ai-sdk/anthropic";
 import { openai } from "@ai-sdk/openai";
 
 const models = {
+  "gpt-4o-2024-08-06": openai("gpt-4o-2024-08-06"),
   "gpt-4o": openai("gpt-4o"),
   "gpt-4o-mini": openai("gpt-4o-mini"),
   "claude-3-5-sonnet-20240620": anthropic("claude-3-5-sonnet-20240620"),
@@ -10,8 +11,8 @@ const models = {
 export const getModel = (name: string) => {
   if (!models[name]) {
     console.log(`Model ${name} not found`);
-    console.log(`Defaulting to gpt-4o`);
-    return models["gpt-4o"];
+    console.log(`Defaulting to gpt-4o-2024-08-06`);
+    return models["gpt-4o-2024-08-06"];
   }
   console.log(`Using model ${models[name]}`);
   return models[name];
