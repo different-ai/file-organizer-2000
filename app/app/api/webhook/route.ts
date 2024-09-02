6a452a4a36c2c6e9e36d990c3b0aeae8c89a40bd
Benjamin Shafii <ben@prologe.io>
2024-09-02 13:54:46 +0200

    feat: reinstate clerk state

--- app/app/api/webhook/route.ts
+++ app/app/api/webhook/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import { createOrUpdateUserSubscriptionStatus, handleFailedPayment } from "@/drizzle/schema";
+import { clerkClient } from "@clerk/nextjs/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-04-10",
@@ -42,6 +43,27 @@ export async function POST(req: NextRequest) {
       console.log(session.status);
       console.log(session.payment_status);
       console.log('session.mode', session.mode);
+      try {
+        console.log(
+          "updating clerk metadata for user",
+          session.metadata?.userId
+        );
+        await clerkClient.users.updateUserMetadata(
+          event.data.object.metadata?.userId as string,
+          {
+            publicMetadata: {
+              stripe: {
+                // stripe customer id
+                customerId: session.customer,
+                status: session.status,
+                payment: session.payment_status,
+              },
+            },
+          }
+        );
+      } catch (error) {
+        console.error(`Error updating user metadata: ${error}`);
+      }
 
       const billingCycle = session.mode === 'subscription' ? 'monthly' : 'lifetime';
 
