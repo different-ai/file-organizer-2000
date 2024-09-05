ccd9e351fcac03a98dadf649d0e3784e02a16ddf
Alexandre Shafii <46509400+aexshafii@users.noreply.github.com>
2024-09-05 21:16:15 +0200

    fix: bugwith double # before tags + suggesting tags already in content (#225)

--- plugin/views/organizer/view.tsx
+++ plugin/views/organizer/view.tsx
@@ -81,7 +81,7 @@ const SimilarTags: React.FC<{
               className={`tag ${existingTags?.includes(tag) ? 'existing-tag' : 'new-tag'}`}
               onClick={() => plugin.appendTag(file!, tag)}
             >
-              #{tag}
+        #{tag.replace(/^#+/, '')}
             </span>
           ))}
           {allTags.length === 0 && (
