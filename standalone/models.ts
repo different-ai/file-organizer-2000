c4140abc09c1a9e4658aed8738c45e126a5bdd6b
Benjamin Shafii <ben@prologe.io>
2024-09-13 20:23:45 +0200

    chore: delete unused code

--- standalone/models.ts
+++ standalone/models.ts
@@ -1,13 +1,5 @@
-import { createAnthropic } from "@ai-sdk/anthropic";
-import { createOpenAI, OpenAIProvider } from "@ai-sdk/openai";
 import { LanguageModel } from "ai";
-import { createOllama } from "ollama-ai-provider";
 
-interface ModelConfig {
-  [key: string]: any;
-  defaultObjectGenerationMode: string;
-  modelId: string;
-}
 
 type Models = {
   [key: string]: LanguageModel;
@@ -17,39 +9,9 @@ export const models: Models = {};
 
 const taskModelConfig: Record<string, string> = {};
 
-export function createOpenAIInstance(
-  apiKey: string,
-  modelName = "gpt-4o",
-  baseURL = "https://api.openai.com/v1"
-) {
-  console.log("initializing openai instance", modelName, apiKey, baseURL);
-  const modelInstance = createOpenAI({ apiKey, baseURL })(modelName);
-  models[modelName] = modelInstance;
-  return modelInstance;
-}
 
-export function createAnthropicInstance(apiKey: string, modelName: string) {
-  const modelInstance = createAnthropic({ apiKey })(modelName);
-  models[modelName] = modelInstance;
-  return modelInstance;
-}
 
-export function createOllamaInstance(
-  modelName: string,
-  { baseURL = "http://localhost:11434/api" }: { baseURL: string }
-) {
-  const modelInstance = createOllama({ baseURL })(modelName);
-  models[modelName] = modelInstance;
-  return modelInstance;
-}
 
-export function configureTask(task: string, modelName: string) {
-  console.log(`Configuring task '${task}' with model '${modelName}'`);
-  if (!models[modelName]) {
-    throw new Error(`Model '${modelName}' is not available.`);
-  }
-  taskModelConfig[task] = modelName;
-}
 
 export function getModelFromTask(task: string) {
   const modelName = taskModelConfig[task];
