8c2e5bcd1470fae07f3f367db25253a301d01dd6
Benjamin Shafii <ben@prologe.io>
2024-09-28 13:34:39 +0200

    revert back to npm

--- web/components/ui/checkout-button.tsx
+++ web/components/ui/checkout-button.tsx
@@ -1,12 +1,9 @@
 "use client";
 import { useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
 import { Button } from "./button";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-);
+
 
 export default function CheckoutButton() {
   const [loading, setLoading] = useState(false);
--- web/lib/srm.ts
+++ web/lib/srm.ts
@@ -3,7 +3,7 @@ import { config } from "../srm.config";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2022-11-15",
 });
 
 type Config = typeof config;
