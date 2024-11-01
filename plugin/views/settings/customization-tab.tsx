da11dc188bbe666d07ebaf5295bb24326eee6281
aexshafii <alexandre.shafii@gmail.com>
2024-11-01 14:53:46 +0100

    chore: remove duplicate enable fabric toggle

--- plugin/views/settings/customization-tab.tsx
+++ plugin/views/settings/customization-tab.tsx
@@ -82,13 +82,7 @@ export const CustomizationTab: React.FC<CustomizationTabProps> = ({ plugin }) =>
       />
 
       <h3 className="text-lg font-semibold mt-6 mb-2">Experimental features</h3>
-{/* 
-      <ToggleSetting
-        name="Enable Fabric-like Formatting"
-        description="Use Fabric-like prompt structure for document formatting."
-        value={enableFabric}
-        onChange={(value) => handleToggleChange(value, setEnableFabric, 'enableFabric')}
-      /> */}
+
       <ToggleSetting
         name="Use Local Chat"
         description="Toggle to use local chat instead of server-based chat"
