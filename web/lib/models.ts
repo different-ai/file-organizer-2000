84174f6cbca9a47222196fbe95fc0d65f3561baf
Benjamin Shafii <ben@prologe.io>
2024-11-08 16:29:29 +0100

    feat(api): add sonnet suppport

--- web/lib/models.ts
+++ web/lib/models.ts
@@ -6,6 +6,7 @@ const models = {
   "gpt-4o-2024-08-06": openai("gpt-4o-2024-08-06", ),
   "gpt-4o-mini": openai("gpt-4o-mini"),
   "claude-3-5-sonnet-20240620": anthropic("claude-3-5-sonnet-20240620"),
+  "claude-3-5-sonnet-20241022": anthropic("claude-3-5-sonnet-20241022"),
 };
 
 export const getModel = (name: string) => {
